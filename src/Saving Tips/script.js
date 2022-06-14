0433b5318332acbbc3e9312b881cb1d778772f10
WalberJC <103972585+WalberJC@users.noreply.github.com>
2022-06-13 22:03:33 -0300

    update

--- src/Saving Tips/script.js
+++
@@ -1,6 +0,0 @@
-let simulaInvestimento = document.querySelector('.simula-investimento');
-let tudo = document.querySelector('.tudo');
-
-    simulaInvestimento.onclick = function() {
-        tudo.classList.toggle('active');
-    }
